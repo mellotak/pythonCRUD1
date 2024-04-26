df7b33926556404ee090a01989c6955828dc36ca
sunombre1 <suemail1@gmail.com>
2024-04-26 15:01:27 -0500

    Implementacion interface del arbol de datos

--- crudUNO.py
+++ crudUNO.py
@@ -4,10 +4,23 @@ from tkinter import ttk
 class ProductCRUDApp:
     def __init__(self, root):
         self.root = root
+        self.root.title("Gestión de Productos")
 
-# Crear una etiqueta para mostrar "Hola Mundo"
-        label = ttk.Label(self.root, text="Hola Mundo")
-        label.pack(padx=150, pady=20)
+        self.create_widgets()
+        
+    
+    def create_widgets(self):
+        self.create_treeview()
+
+    def create_treeview(self):
+        self.tree = ttk.Treeview(self.root, columns=("ID", "Nombre", "Precio", "Stock"), show="headings")
+        self.tree.heading("ID", text="ID")
+        self.tree.heading("Nombre", text="Nombre")
+        self.tree.heading("Precio", text="Precio")
+        self.tree.heading("Stock", text="Stock")
+        self.tree.pack(padx=10, pady=10)
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
