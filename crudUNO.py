aeee0c53a1a3a8f52677e072e160307d24bfa08a
sunombre1 <suemail1@gmail.com>
2024-04-26 12:42:35 -0500

    Activar ventana del proyecto

---
+++ crudUNO.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+class ProductCRUDApp:
+    def __init__(self, root):
+        self.root = root
+
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ProductCRUDApp(root)
+    root.mainloop()
