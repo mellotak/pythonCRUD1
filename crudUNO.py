f3a61dadb16a4be8ac36c6ff168c1766140786f6
sunombre1 <suemail1@gmail.com>
2024-04-26 12:45:50 -0500

    Hola mundo, en ventana emergente

--- crudUNO.py
+++ crudUNO.py
@@ -5,9 +5,9 @@ class ProductCRUDApp:
     def __init__(self, root):
         self.root = root
 
-
-
-
+# Crear una etiqueta para mostrar "Hola Mundo"
+        label = ttk.Label(self.root, text="Hola Mundo")
+        label.pack(padx=150, pady=20)
 
 if __name__ == "__main__":
     root = tk.Tk()
