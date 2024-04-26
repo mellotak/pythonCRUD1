f61836fed7bf8b604c36f2703f252cceef09b260
sunombre1 <suemail1@gmail.com>
2024-04-26 15:10:52 -0500

    Implementacion de input, campos de entrada

--- crudUNO.py
+++ crudUNO.py
@@ -7,10 +7,11 @@ class ProductCRUDApp:
         self.root.title("Gestión de Productos")
 
         self.create_widgets()
-        
+
     
     def create_widgets(self):
         self.create_treeview()
+        self.create_input_fields()
 
     def create_treeview(self):
         self.tree = ttk.Treeview(self.root, columns=("ID", "Nombre", "Precio", "Stock"), show="headings")
@@ -19,6 +20,17 @@ class ProductCRUDApp:
         self.tree.heading("Precio", text="Precio")
         self.tree.heading("Stock", text="Stock")
         self.tree.pack(padx=10, pady=10)
+
+    def create_input_fields(self):
+        fields = [("Nombre del Producto:", 10), ("Precio:", 10), ("Stock:", 10)]
+        self.entries = {}
+        for label_text, width in fields:
+            label = ttk.Label(self.root, text=label_text)
+            label.pack(pady=(0, 5), padx=10, anchor="w")
+
+            entry = ttk.Entry(self.root, width=width)
+            entry.pack(pady=(0, 10), padx=10, fill="x")
+            self.entries[label_text] = entry
 
 
 
